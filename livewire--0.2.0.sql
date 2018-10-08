3559e9ec3f6b081ec37288c1840492d241ca50c4
rhysallister <rhys@auchindown.com>
2018-10-08 01:30:13 -0500

    Added new variants of lw_tracednstream
    Added three new variants of aforemention function. The first allows
    tracing from multiple points. The second allows the additions of
    blocks to stop the trace. The third allows for the passing of a
    point geometry. In this case the nearest node to point will bw used
    to initiate the trace down stream.

--- livewire--0.2.0.sql
+++ livewire--0.2.0.sql
@@ -875,6 +875,105 @@ $lw_tracednstream$
   END;
 
 $lw_tracednstream$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION lw_tracednstream(
+  in lw_schema text,
+  in lw_ids bigint[],
+  out g geometry) as
+
+$lw_tracednstream$
+  DECLARE
+    rec record;
+    qrytxt text;
+    z bigint;
+  BEGIN
+
+    qrytxt := 'CREATE TEMPORARY TABLE __ ON COMMIT DROP AS 
+               SELECT  *, null::bigint[] ne FROM %1$I.__livewire 
+               where %2$L && nodes';
+    EXECUTE format(qrytxt, lw_schema, lw_ids); 
+
+
+    FOREACH z in ARRAY lw_ids LOOP
+
+      UPDATE __ set ne =  CASE 
+        WHEN edges[(array_position(nodes::bigint[], z)):] is null 
+        THEN ne 
+        ELSE edges[(array_position(nodes::bigint[], z)):] end;
+    END LOOP;
+    
+    qrytxt := 'SELECT st_union(g) from %1$I.__lines WHERE lw_id IN
+               (select distinct unnest(ne) FROM __)';
+    EXECUTE format(qrytxt, lw_schema) into g;
+  END;
+
+$lw_tracednstream$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION lw_tracednstream(
+  in lw_schema text,
+  in lw_ids bigint[],
+  in bl_ids bigint[],
+  out g geometry) as
+
+$lw_tracednstream$
+  DECLARE
+    rec record;
+    qrytxt text;
+    z bigint;
+  BEGIN
+
+    qrytxt := 'CREATE TEMPORARY TABLE __ ON COMMIT DROP AS 
+               SELECT  *, null::bigint[] n, null::bigint[] e 
+               FROM %1$I.__livewire WHERE %2$L && nodes';
+    EXECUTE format(qrytxt, lw_schema, lw_ids);
+
+
+    FOREACH z in ARRAY lw_ids LOOP
+
+      UPDATE __ set e =  CASE
+        WHEN edges[(array_position(nodes::bigint[], z)):] is null
+        THEN e
+        ELSE edges[(array_position(nodes::bigint[], z)):] end,
+      n = CASE WHEN nodes[array_position(nodes::bigint[], z):] is null
+        THEN n
+        ELSE nodes[array_position(nodes::bigint[], z):] END;
+    END LOOP;
+
+    FOREACH z in ARRAY bl_ids LOOP
+      UPDATE __ set e =  CASE
+        WHEN e[:(array_position(n::bigint[], z))] is null
+        THEN e
+        ELSE e[:(array_position(n::bigint[], z)) - 1] end;
+    END LOOP;
+    
+    qrytxt := 'SELECT st_union(g) from %1$I.__lines WHERE lw_id IN
+               (select distinct unnest(e) FROM __)';
+    EXECUTE format(qrytxt, lw_schema) into g;
+  END;
+
+
+$lw_tracednstream$ LANGUAGE plpgsql;
+
+CREATE FUNCTION lw_tracednstream(
+  in lw_schema text,
+  in in_g geometry,
+  out g geometry) as
+
+$lw_tracednstream$
+  DECLARE
+    qrytxt text;
+
+  BEGIN
+    qrytxt := 'SELECT lw_tracednstream(%1$L,lw_id)  FROM %1$I.__nodes
+              ORDER BY g <-> %2$L LIMIT 1';
+   EXECUTE format(qrytxt, lw_schema, in_g) INTO g;
+
+  END;
+
+$lw_tracednstream$ LANGUAGE plpgsql;
+
 /*    Given a source lw_id, trace a feeder and populate __livewire    */
 
 CREATE OR REPLACE FUNCTION lw_tracesource(
